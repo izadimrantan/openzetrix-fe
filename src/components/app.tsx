4b874bf6850c02d0a84be440c8606faf15242fbf
Izad Imran Tan <izad.mhdborhan@myeg.com.my>
2024-11-05 04:16:25 -0500

    Added Google Analytics

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,36 +1,50 @@
-"use client"
+"use client";
 
 import { getLocalStorageItem } from "@/libs/core";
 import { createContext, useContext, useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import ReactGA from "react-ga4";
 
-// Context is a way to manage state globally.
-const Context = createContext<any>(null)
+// Initialize Google Analytics with your Measurement ID
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID; // Replace with your Measurement ID
+if (GA_MEASUREMENT_ID) {
+    ReactGA.initialize(GA_MEASUREMENT_ID);
+} else {
+    console.error("Google Analytics initializing error.")
+}
+
+const Context = createContext<any>(null);
 
 export default function AppContext(props: any) {
-    useEffect(() => {
-        // Load application context from storage
-        setWalletAddress(getLocalStorageItem("walletAddress", ""))
-    }, [])
+  const pathname = usePathname();
+  const [walletAddress, setWalletAddress] = useState<string>("");
+  const [contractType, setContractType] = useState<string>("ZTP20");
 
-    // List of state that need to be use globally
-    const [walletAddress, setWalletAddress] = useState<string>("")
-    const [contractType, setContractType] = useState<string>("ZTP20")
+  useEffect(() => {
+    // Load application context from storage
+    setWalletAddress(getLocalStorageItem("walletAddress", ""));
 
-    // Set value for context
-    const contextValue = {
-        walletAddress,
-        setWalletAddress,
-        contractType,
-        setContractType
+    // Track page view on pathname change
+    if (pathname) {
+      ReactGA.send({ hitType: "pageview", page: pathname });
     }
+  }, [pathname]);
 
-    return (
-        <Context.Provider value={contextValue}>
-            <div className="min-h-screen">{props.children}</div>
-        </Context.Provider>
-    );
+  // Set value for context
+  const contextValue = {
+    walletAddress,
+    setWalletAddress,
+    contractType,
+    setContractType,
+  };
+
+  return (
+    <Context.Provider value={contextValue}>
+      <div className="min-h-screen">{props.children}</div>
+    </Context.Provider>
+  );
 }
 
 export function useAppContext() {
-    return useContext(Context)
+  return useContext(Context);
 }
